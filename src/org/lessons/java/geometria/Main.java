2fc9c7bbf1da59781aeb560d3044e447acfa3058
Pietromuresu <muresupietro66@gmail.com>
2023-09-15 14:51:12 +0200

    Create Rettangolo class and its constructor

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -3,6 +3,9 @@ package org.lessons.java.geometria;
 public class Main {
 	
 	public static void main(String[] args) {
+		Rettangolo rettangolo1 = new Rettangolo(20, 10);
 		
+		System.out.println("Base: " + rettangolo1.base);
+		System.out.println("Height: " + rettangolo1.height);
 	}
 }
---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,14 @@
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+		public int base;
+		public int height;
+		
+		public Rettangolo(int base, int height) {
+			this.base = base;
+			this.height = height;
+		}
+		
+		
+}
