02afc3bb119eac5f319233704066d887479f9d43
Pietromuresu <muresupietro66@gmail.com>
2023-09-15 15:25:20 +0200

    Calculate the area and perimeter for the given geometries parameters

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,14 +1,52 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		Rettangolo rettangolo1 = new Rettangolo(20, 10);
 		
-		System.out.println("Base: " + rettangolo1.base);
-		System.out.println("Height: " + rettangolo1.height);
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Area: " + rettangolo1.calculateArea());
-		System.out.println("Perimeter: " + rettangolo1.calculatePerimeter());
+		int limit = 3;
+		
+		Rettangolo rettangolo1 = null;
+		Rettangolo rettangolo2 = null;
+		Rettangolo rettangolo3 = null;
+		
+		for(int i = 1; i < limit + 1; i++) {
+			String rettangoloName = "rettangolo" + i;
+			
+			System.out.print("Define an heigth for " + rettangoloName + ": " );
+			int height = sc.nextInt();
+
+			System.out.print("Define the base length for " + rettangoloName  + ": " );
+			int base = sc.nextInt();
+			
+			if(i == 1) {
+				
+				rettangolo1 = new Rettangolo(base, height); 
+			}else if(i == 2) {
+				
+				rettangolo2 = new Rettangolo(base, height); 	
+			}else {
+				
+				rettangolo3 = new Rettangolo(base, height); 
+			}
+		}
+		
+		System.out.println("\n Rettangolo 1 --------------------------------------- ");
+		System.out.println("Rettangolo 1 Area: " + rettangolo1.calculateArea());
+		System.out.println("Rettangolo 1 Perimeter: " + rettangolo1.calculatePerimeter());
+		
+		System.out.println("\n Rettangolo 2 --------------------------------------- ");
+		System.out.println("Rettangolo 2 Area: " + rettangolo2.calculateArea());
+		System.out.println("Rettangolo 2 Perimeter: " + rettangolo2.calculatePerimeter());
+		
+		System.out.println("\n Rettangolo 3 ---------------------------------------  ");
+		System.out.println("Rettangolo 3 Area: "	+ rettangolo3.calculateArea());
+		System.out.println("Rettangolo 3 Perimeter: "	+ rettangolo3.calculatePerimeter());
+		
+		
 	}
 }
